d9990112fb188d0fa56b67e5ec1ce924cede486c
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2017-09-26 09:21:57 +0000

    Zips updated

--- src/main/java/istat/android/base/tools/Zips.java
+++ src/main/java/istat/android/base/tools/Zips.java
@@ -16,186 +16,169 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 /**
- * 
  * @author toshiba1
  */
 public class Zips {
 
-	public static void unZip(String inputFile, String outputFile) {
-		UnZipper.unZip(new File(inputFile), new File(outputFile));
-	}
+    public static void unZip(String inputFile, String outputFile) throws IOException {
+        UnZipper.unZip(new File(inputFile), new File(outputFile));
+    }
 
-	public static void zip(String inputFile, String outputFile) {
-		Zipper.zip(new File(inputFile), new File(outputFile));
-	}
+    public static void zip(String inputFile, String outputFile) throws IOException {
+        Zipper.zip(new File(inputFile), new File(outputFile));
+    }
 
-	public static class UnZipper {
+    public static class UnZipper {
 
-		public static void unZip(File inputFile, File outputFile) {
-			try {
-				ZipFile zipFile = new ZipFile(inputFile);
-				Enumeration<?> enu = zipFile.entries();
-				while (enu.hasMoreElements()) {
-					ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+        public static void unZip(File inputFile, File outputFile) throws IOException {
+            ZipFile zipFile = new ZipFile(inputFile);
+            Enumeration<?> enu = zipFile.entries();
+            while (enu.hasMoreElements()) {
+                ZipEntry zipEntry = (ZipEntry) enu.nextElement();
 
-					String name = zipEntry.getName();
-					long size = zipEntry.getSize();
-					long compressedSize = zipEntry.getCompressedSize();
-					System.out.printf(
-							"name: %-20s | size: %6d | compressed size: %6d\n",
-							name, size, compressedSize);
+                String name = zipEntry.getName();
+                long size = zipEntry.getSize();
+                long compressedSize = zipEntry.getCompressedSize();
+                System.out.printf(
+                        "name: %-20s | size: %6d | compressed size: %6d\n",
+                        name, size, compressedSize);
 
-					File file = new File(outputFile.getAbsoluteFile() + "/"
-							+ name);
-					if (name.endsWith("/")) {
-						file.mkdirs();
-						continue;
-					}
+                File file = new File(outputFile.getAbsoluteFile() + "/"
+                        + name);
+                if (name.endsWith("/")) {
+                    file.mkdirs();
+                    continue;
+                }
 
-					File parent = file.getParentFile();
-					if (parent != null) {
-						parent.mkdirs();
-					}
+                File parent = file.getParentFile();
+                if (parent != null) {
+                    parent.mkdirs();
+                }
 
-					InputStream is = zipFile.getInputStream(zipEntry);
-					FileOutputStream fos = new FileOutputStream(file);
-					byte[] bytes = new byte[1024];
-					int length;
-					while ((length = is.read(bytes)) >= 0) {
-						fos.write(bytes, 0, length);
-					}
-					is.close();
-					fos.close();
+                InputStream is = zipFile.getInputStream(zipEntry);
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] bytes = new byte[1024];
+                int length;
+                while ((length = is.read(bytes)) >= 0) {
+                    fos.write(bytes, 0, length);
+                }
+                is.close();
+                fos.close();
 
-				}
-				zipFile.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            }
+            zipFile.close();
+        }
+    }
 
-	public static class Zipper {
+    public static class Zipper {
 
-		private static void getAllFiles(File dir, List<File> fileList) {
-			try {
-				File[] files = dir.listFiles();
-				for (File file : files) {
-					fileList.add(file);
-					if (file.isDirectory()) {
-						System.out.println("directory:"
-								+ file.getCanonicalPath());
-						getAllFiles(file, fileList);
-					} else {
-						System.out.println("     file:"
-								+ file.getCanonicalPath());
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+        private static void getAllFiles(File dir, List<File> fileList) throws IOException {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                fileList.add(file);
+                if (file.isDirectory()) {
+                    System.out.println("directory:"
+                            + file.getCanonicalPath());
+                    getAllFiles(file, fileList);
+                } else {
+                    System.out.println("     file:"
+                            + file.getCanonicalPath());
+                }
+            }
 
-		private static void writeZipFile(File directoryToZip,
-				List<File> fileList, File directoryOutput) {
+        }
 
-			try {
-				FileOutputStream fos =null;
-				
-				if(directoryOutput.isDirectory()){
-					 fos = new FileOutputStream(directoryOutput
-					+ "/" + directoryToZip.getName() + ".zip");
-				}else
-					fos = new FileOutputStream(directoryOutput);
+        private static void writeZipFile(File directoryToZip,
+                                         List<File> fileList, File directoryOutput) throws IOException {
 
-				ZipOutputStream zos = new ZipOutputStream(fos);
+            FileOutputStream fos;
 
-				for (File file : fileList) {
-					if (!file.isDirectory()) { // we only zip files, not
-												// directories
-						addToZip(directoryToZip, file, zos);
-					}
-				}
+            if (directoryOutput.isDirectory()) {
+                fos = new FileOutputStream(directoryOutput
+                        + "/" + directoryToZip.getName() + ".zip");
+            } else
+                fos = new FileOutputStream(directoryOutput);
 
-				zos.close();
-				fos.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+            ZipOutputStream zos = new ZipOutputStream(fos);
 
-		public static void addToZip(File directoryToZip, File file,
-				ZipOutputStream zos) throws FileNotFoundException, IOException {
-			try {
-				FileInputStream fis = new FileInputStream(file);
-				String zipFilePath = file.getCanonicalPath().substring(
-						directoryToZip.getCanonicalPath().length() + 1,
-						file.getCanonicalPath().length());
-				System.out.println("Writing '" + zipFilePath + "' to zip file");
-				ZipEntry zipEntry = new ZipEntry(zipFilePath);
-				zos.putNextEntry(zipEntry);
+            for (File file : fileList) {
+                if (!file.isDirectory()) { // we only zip files, not
+                    // directories
+                    addToZip(directoryToZip, file, zos);
+                }
+            }
 
-				byte[] bytes = new byte[1024];
-				int length;
-				while ((length = fis.read(bytes)) >= 0) {
-					zos.write(bytes, 0, length);
-				}
-			} catch (IOException e) {
+            zos.close();
+            fos.close();
+        }
 
-			}
-			zos.closeEntry();
-		}
+        public static void addToZip(File directoryToZip, File file,
+                                    ZipOutputStream zos) throws IOException {
+            FileInputStream fis = new FileInputStream(file);
+            String zipFilePath = file.getCanonicalPath().substring(
+                    directoryToZip.getCanonicalPath().length() + 1,
+                    file.getCanonicalPath().length());
+            System.out.println("Writing '" + zipFilePath + "' to zip file");
+            ZipEntry zipEntry = new ZipEntry(zipFilePath);
+            zos.putNextEntry(zipEntry);
 
-		public static void zip(File inputFile, File outputFile) {
-			List<File> fileList = new ArrayList<File>();
-			// System.out.println("---Getting references to all files in: " +
-			// directoryToZip.getCanonicalPath());
-			getAllFiles(inputFile, fileList);
-			// System.out.println("---Creating zip file");
-			writeZipFile(inputFile, fileList, outputFile);
-			// System.out.println("---Done");
-		}
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = fis.read(bytes)) >= 0) {
+                zos.write(bytes, 0, length);
+            }
 
-		public static void zip(File[] files, String zipFile) throws IOException {
-			String[] _files = new String[files.length];
-			int i = 0;
-			for (File file : files) {
-				_files[i] = file.getAbsolutePath();
-				i++;
+            zos.closeEntry();
+        }
 
-			}
-			zip(_files, zipFile);
-		}
+        public static void zip(File inputFile, File outputFile) throws IOException {
+            List<File> fileList = new ArrayList<File>();
+            // System.out.println("---Getting references to all files in: " +
+            // directoryToZip.getCanonicalPath());
+            getAllFiles(inputFile, fileList);
+            // System.out.println("---Creating zip file");
+            writeZipFile(inputFile, fileList, outputFile);
+            // System.out.println("---Done");
+        }
 
-		public static void zip(String[] files, String zipFile)
-				throws IOException {
-			int BUFFER_SIZE = 1024;
-			BufferedInputStream origin = null;
-			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(
-					new FileOutputStream(zipFile)));
-			try {
-				byte data[] = new byte[BUFFER_SIZE];
+        public static void zip(File[] files, String zipFile) throws IOException {
+            String[] _files = new String[files.length];
+            int i = 0;
+            for (File file : files) {
+                _files[i] = file.getAbsolutePath();
+                i++;
 
-				for (int i = 0; i < files.length; i++) {
-					FileInputStream fi = new FileInputStream(files[i]);
-					origin = new BufferedInputStream(fi, BUFFER_SIZE);
-					try {
-						ZipEntry entry = new ZipEntry(
-								files[i].substring(files[i].lastIndexOf("/") + 1));
-						out.putNextEntry(entry);
-						int count;
-						while ((count = origin.read(data, 0, BUFFER_SIZE)) != -1) {
-							out.write(data, 0, count);
-						}
-					} finally {
-						origin.close();
-					}
-				}
-			} finally {
-				out.close();
-			}
-		}
-	}
+            }
+            zip(_files, zipFile);
+        }
+
+        public static void zip(String[] files, String zipFile)
+                throws IOException {
+            int BUFFER_SIZE = 1024;
+            BufferedInputStream origin;
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(
+                    new FileOutputStream(zipFile)));
+            //  try {
+            byte data[] = new byte[BUFFER_SIZE];
+
+            for (int i = 0; i < files.length; i++) {
+                FileInputStream fi = new FileInputStream(files[i]);
+                origin = new BufferedInputStream(fi, BUFFER_SIZE);
+                //  try {
+                ZipEntry entry = new ZipEntry(
+                        files[i].substring(files[i].lastIndexOf("/") + 1));
+                out.putNextEntry(entry);
+                int count;
+                while ((count = origin.read(data, 0, BUFFER_SIZE)) != -1) {
+                    out.write(data, 0, count);
+                }
+                // } finally {
+                origin.close();
+                //}
+            }
+            // } finally {
+            out.close();
+            //  }
+        }
+    }
 }
